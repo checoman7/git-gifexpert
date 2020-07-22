3f68b8fe32e1cf3712a278cec065a13a410d4609
Garcia <sergio.b.garcia@accenture.com>
2020-07-22 09:17:10 -0500

    git docs

---
+++ docs/precache-manifest.41207d5c3a770d7a0db13d6100451f8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23bfa6a345c03b77902958f677386c51",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ea6486bff018151a35cb",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea6486bff018151a35cb",
+    "url": "/static/js/main.798e59d0.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
